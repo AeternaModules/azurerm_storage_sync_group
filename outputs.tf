e71653e5624a1aa84594749e7d8b779ab592e46a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "storage_sync_groups" {
-  description = "All storage_sync_group resources"
-  value       = azurerm_storage_sync_group.storage_sync_groups
-}
 output "storage_sync_groups_name" {
-  description = "List of name values across all storage_sync_groups"
-  value       = [for k, v in azurerm_storage_sync_group.storage_sync_groups : v.name]
+  description = "Map of name values across all storage_sync_groups, keyed the same as var.storage_sync_groups"
+  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.name }
 }
 output "storage_sync_groups_storage_sync_id" {
-  description = "List of storage_sync_id values across all storage_sync_groups"
-  value       = [for k, v in azurerm_storage_sync_group.storage_sync_groups : v.storage_sync_id]
+  description = "Map of storage_sync_id values across all storage_sync_groups, keyed the same as var.storage_sync_groups"
+  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.storage_sync_id }
 }
 
