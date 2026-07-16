a5dab153fa815fa0ef36b0f5d06b80104e2e5389
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "storage_sync_groups_id" {
   description = "Map of id values across all storage_sync_groups, keyed the same as var.storage_sync_groups"
-  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.id }
+  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_sync_groups_name" {
   description = "Map of name values across all storage_sync_groups, keyed the same as var.storage_sync_groups"
-  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.name }
+  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "storage_sync_groups_storage_sync_id" {
   description = "Map of storage_sync_id values across all storage_sync_groups, keyed the same as var.storage_sync_groups"
-  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.storage_sync_id }
+  value       = { for k, v in azurerm_storage_sync_group.storage_sync_groups : k => v.storage_sync_id if v.storage_sync_id != null && length(v.storage_sync_id) > 0 }
 }
 
